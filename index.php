3a25b9760dd5a9f1809bb5fbf50a57f3e257bd2b
esposito.00@outlook.it <esposito.00@outlook.it>
2023-01-24 19:02:24 +0100

    aggiunta composizione

--- index.php
+++ index.php
@@ -1,33 +1,65 @@
 <?php
 
 // Classe
+class Credits
+{
+    // Variabili/Attributi d'istanza
+    public $director;
+    public $producer;
+    public $screenwriter;
+
+    // Costruttore
+    public function __construct($_director, $_producer, $_screenwriter)
+    {
+        $this->director = $_director;
+        $this->producer = $_producer;
+        $this->screenwriter = $_screenwriter;
+    }
+
+    // Metodo/Funzione
+    public function getCredits()
+    {
+        return '<br> - director:  ' . $this->director
+            . '<br> - producer: ' . $this->producer
+            . '<br> - screenwriter: ' . $this->screenwriter;
+    }
+}
+
+
+// Classe
 class Movie
 {
     // Variabili/Attributi d'istanza
     public $title;
     public $genre;
-
+    public Credits $credits; // Composizione
 
     // Costruttore
-    public function __construct($_title, $_genre)
+    public function __construct($_title, $_genre, Credits $_credits)
     {
         $this->title = $_title;
         $this->genre = $_genre;
+        $this->credits = $_credits;
     }
-
 
     // Metodo/Funzione
     public function getHtml()
     {
         return 'Movie: ' . $this->title
-            . '<br> Genre: ' . $this->genre;
+            . '<br> Genre: ' . $this->genre
+            . '<br> Credits: ' . $this->credits->getCredits();
 
     }
 }
 
+
 // Oggetti/Istanze
-$joker = new Movie('Joker', 'Crime');
-$cmbyn = new Movie('Call Me By Your Name', 'Romantic');
+$creditsJoker = new Credits('Todd Phillips', 'Bradley Cooper', 'Scott Silver');
+$creditsCmbyn = new Credits('Luca Guadagnino', 'Peter Spears', '/');
+
+$joker = new Movie('Joker', 'Crime', $creditsJoker);
+$cmbyn = new Movie('Call Me By Your Name', 'Romantic', $creditsCmbyn);
+
 
 echo '1 <br>' . $joker->getHtml()
     . '<br><br>';
